41cba745190acd8c01f2d0565416e0778c8810cb
Nikhil Gupta <me@nikhgupta.com>
2015-10-05 12:54:43 +0530

    app does no longer allow registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   has_many :workables, foreign_key: :locked_by, class_name: "Place"
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   def to_s
     email
